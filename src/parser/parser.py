7979a93ca67b149a12d090ba684ecee2c2cf83cb
Hisao <jaimehisao2011@gmail.com>
2020-07-23 23:50:44 +0000

    Basic parser functionality added

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,59 +1,52 @@
-from PyPDF2 import PdfFileReader
-from pprint import pprint
-
-def text_extractor():
-    pass
-
-def remove_extra_spaces():
-    pass
-
-def remove_footer():
-    pass
-
-def separate_into_articles():
-    pass
-
-def text_extractor(path):
-    with open(path, 'rb') as f:
-        pdf = PdfFileReader(f)
-
-        last = None
-
-        for page in pdf.pages:
-            last = page.extractText()
-
-            print(last)
-
-            # Remove '/n' from all string elements.
-            page = last.replace(' \n', '')
-            page = page.translate({ord('\n'): None})
-
-            # Remove extra spaces
-            # page = page.replace('  ', ' ')
-            # page = page.translate({ord(i): ' ' for i in '  '})
-
-            last = page
-            # pprint(last)
-            # print(last)
-            
-            # pprint(page.extractText())
-            # print(type(page))
-            # print(type(page.extractText()))
-        # print(type(pdf))
+import slate
 
 
+import logging
+
+# import numpy
+
+logging.basicConfig(level=logging.INFO)
 
 
-
-def get_info(path):
-    with open(path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        info = pdf.getDocumentInfo()
-        n_pages = pdf.getNumPages()
-    print(info)
-    print(n_pages)
+class Article:
+    def __init__(self, number, text):
+        self.number = number
+        self.text = text
 
 
-if __name__ == '__main__':
-    path = 'regs.pdf'
-    text_extractor(path)
+def count_articles(pdf_text):
+    articles = []
+    article_text = ""
+    article_count = 1
+    i = 0
+    while i < len(pdf_text) - 1:
+        if (pdf_text[i] == "artículo" or pdf_text[i] == "articulo") and (
+            pdf_text[i + 1] == str(article_count) + ".-"
+            or pdf_text[i + 1] == str(article_count) + "-"
+            or pdf_text[i + 1] == str(article_count) + "."
+        ):
+            logging.info("Article #" + str(article_count) + " recognized!")
+            articles.append(Article(article_count, article_text))
+            article_text = ""
+            article_count += 1
+            i += 1
+        else:
+            article_text += " " + pdf_text[i]
+        i += 1
+    for article in articles:
+        logging.info("Article: " + str(article.number - 1) + " Text: " + article.text)
+
+
+with open("regs.pdf", "rb") as f:
+    doc = slate.PDF(f)
+
+    final_text = ""
+    for page in doc:
+        why = page
+        final_text += page
+    final_text = final_text.strip().lower().split()
+    # final_text_pass = final_text
+    # final_text = numpy.array(final_text)
+    # numpy.savetxt("foo.txt", final_text, fmt="%s")
+    # print(final_text)
+    count_articles(final_text)
