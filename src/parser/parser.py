9d1146b59564048405b97145df2eb6a661d269c5
Hisao <jaimehisao2011@gmail.com>
2020-08-12 01:32:33 +0000

    Merged Parser w/DB and w/out DB

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -51,6 +51,7 @@ class Article:
         self.number = number
         self.text = text
 
+
 class Keyword:
     """Class for storing keywords.
     """
@@ -58,6 +59,16 @@ class Keyword:
     def __init__(self, number, text):
         self.number = number
         self.text = text
+
+
+articles_in_memory = []
+
+
+def get_article_by_number(artNum):
+    for item in articles_in_memory:
+        if artNum == str(item["articleNumber"]):
+            return item
+    return "No article matches such ID", 402
 
 
 def count_articles(pdf_text):
@@ -93,53 +104,57 @@ def count_articles(pdf_text):
     return articles
 
 
+keywords = {}
+documents = {
+    'hash': 'afafbfbdce8c40924edae00f6ce54f0c639ce42a2c0fbbfa6ab82ea6925827c51',  # Added one at last
+    'jurisdiction': 'Monterrey',
+    'url': 'http://www.guadalupe.gob.mx/wp-content/uploads/2019/09/Nuevo-Reglamento-Homologado-1.pdf'
+ }
+document_list = []
+document_list.append(documents)
+
+
 def parse():
     """Parses all PDF documents that are in the DB"""
-    docs = database.get_regulation_documents()
-    for doc in docs:
-        document = doc.to_dict()
-        retriever.get_document(document["url"])
+    # docs = database.get_regulation_documents()  # DB Component
+    for document_to_parse in document_list:
+        # document = doc.to_dict()  # used when using DB Comp
+        retriever.get_document(document_to_parse["url"])  # Change to document when using DB
+
+        # Check if the file is different, this has to be done in a different open file instance
+        # if not, Python returns some weird errors
         hasher = hashlib.sha256()
         with open("regs.pdf", "rb") as pdf_file:
             file_buffer = pdf_file.read()
             hasher.update(file_buffer)
-            if hasher.hexdigest() != document["hash"]:
-                doc = slate.PDF(pdf_file)
-                final_text = ""
-                for page in doc:
-                    final_text.join(page)
-                final_text = final_text.strip().lower().split()
-                count_articles(final_text)
-            else:
+            if hasher.hexdigest() == document_to_parse["hash"]:
                 print("Downloaded document is the same as the one currently stored")
+                return 'Succesful Operation - File did not change', 200
 
-
-def parse_without_database():
-    """Parses all PDF documents that are in the DB"""
-    articles_in_memory = []
-    keywords = {}
-    with open("regs.pdf", "rb") as pdf_file:
-        doc = slate.PDF(pdf_file)
-        final_text = ""
-        for page in doc:
-            final_text.join(page)
-        final_text = final_text.strip().lower().split()
-        articles = count_articles(final_text)
-
-        for article in articles:
-            # get the keywords
-            # add to dict, add dict to list
-            article["articleNumber"] = article.number
-            article["text"] = article.text
-            article["wordCount"] = len(article.text.split())
-            articles_in_memory.append(article)
-
-            # Get keywords that relate to this article (Javier's service)
-            keywords_service_response = requests.get('localhost:8000', params={'text':article.text}).json()
-            split_article = article.text.split()
-            for keyword in keywords_service_response["keywords"]:
-                frequency = split_article.count(keyword)
-                keywords[keyword] = namedtuple(article.number, frequency)
-
-
-parse_without_database()
+        with open("regs.pdf", "rb") as pdf_file:
+            doc = slate.PDF(pdf_file)
+            final_text = ""
+            for page in doc:
+                final_text += page
+            final_text = final_text.strip().lower().split()
+            articles = count_articles(final_text)
+            for article in articles:
+                articleDict = {}
+                articleDict['articleNumber'] = article.number
+                articleDict['text'] = article.text
+                articleDict['wordCount'] = len(article.text.split())
+                articles_in_memory.append(articleDict)
+                print('Adding ' + str(articleDict))
+                # Get keywords that relate to this article (Javier's service)
+                '''
+                    keywords_service_response = requests.get(
+                    "localhost:8000", params={"text": article.text}
+                    ).json()
+                '''
+                split_article = article.text.split()
+                '''
+                for keyword in keywords_service_response["keywords"]:
+                    frequency = split_article.count(keyword)
+                    keywords[keyword] = namedtuple(article.number, frequency)
+                    '''
+    return 'Successful Operation', 200
