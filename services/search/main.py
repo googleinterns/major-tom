4a95f04fac4bb0c300b4bcf4ef0cd658e5713f69
Domene99 <jadomene99@gmail.com>
2020-08-13 16:09:08 -0500

    Set up NLP search services MVP (#17)
    * Set up components for NLP searching mvp
    
    * Disable import error for external imports
    
    * Write endpoint functions and locally tested them. Move everything into src
    
    * Set up/separate enpoints dockerfile
    
    * Remove old shared services main
    
    * Separate shared utility functions
    
    * add functions framework to requirements and clean up
    
    * Remove run cmd in dockerfiles
    
    * Quick fix for slow runtime
    
    * Add error handling
    
    * Change search class to new db endpoint return schema
    
    * Fix pylint errors
    
    * Addres pylint E302 expected 2 blank lines
    
    * Address all pylint comments
    
    * Raise exceptions to main
    
    * Remove bare exceptions
    
    * Address comments
    
    * Unit test keywords service
    
    * Pylint fixes
    
    * Dockerfile update for production
    
    * Remove explicit exception raising
    
    * Specify port for search

---
+++ services/search/main.py
@@ -0,0 +1,32 @@
+import traceback  # pylint: disable=import-error
+import constants
+from search_engine import SearchEngine
+
+
+def search_service(request):
+    """
+    Parses http json request and returns list of articles and their
+    scores depending on the input query
+    Args:
+        request: http POST body request as json
+    Returns:
+        list of articles with their respective scores
+    """
+    json = request.get_json()
+    if "query" not in json:
+        return {"error": {"message": "ValueError: Expected 'query' field in json body is missing"}}
+
+    query = json["query"]
+    search = SearchEngine(keywords_weight=constants.KEYWORDS_WEIGHT)
+    try:
+        score_per_article = search.query(query)
+    except Exception as e:
+        return {"error": {"message": getattr(e, 'message', str(e)),
+                          "trace": traceback.format_exc()}}
+
+    # sorts dictionary by value in DESC order
+    articles_sorted = [k for k, v in sorted(
+        score_per_article.items(),
+        key=lambda item: item[1], reverse=True)]
+
+    return {"articles": articles_sorted}
---
+++ services/search/search_engine.py
@@ -0,0 +1,98 @@
+import os
+import requests  # pylint: disable=import-error
+import constants
+import testing.test_constants
+import utils
+
+
+class SearchEngine:
+    """
+    This class implements the functions to score every article
+    by the amount of incidences between keywords and synonyms to articles
+
+    Attributes:
+        keywords: A list of keywords
+        synonyms: A list of synonyms for the keywords
+        articles: A list of articles
+        keywords_weight: The weight that a keyword has on the scorig algorithm
+            Optional: default argument as 1
+        synonyms_weight: The weight that a synonym has on the scoring algorithm
+            Optional: default argument as 1
+    """
+    def __init__(self, keywords_weight=1, synonyms_weight=1):
+        """
+        Inits the search engine by loading all articles
+        """
+        self.keywords_weight = keywords_weight
+        self.synonyms_weight = synonyms_weight
+
+    def query(self, query):
+        """
+        Extracts keywords, from keywords endpoint,
+        and synonyms, from local function,
+        to call search query and calculate the score for articles
+        Args:
+            query: query string
+        Returns:
+            A map of the score of every article
+        """
+        query_text = {'text': query}
+        keywords_endpoint = os.getenv('KEYWORDS_ENDPOINT') is not None or 'http://localhost:8081'
+        response = requests.post(keywords_endpoint, json=query_text)
+        response = response.json()
+
+        lan = response['lan']
+
+        if lan not in constants.SUPPORTED_LANGUAGES:
+            raise ValueError(f'{lan} not supported')
+
+        keywords = []
+
+        for token in response['tokens']:
+            keywords.append(token['lemma'])
+
+        synonyms = utils.create_synonym_list_esp(keywords)
+
+        return self._search_query(keywords, synonyms)
+
+    def _calculate_score(self, frequency, weight, words, target_dict):
+        """
+        Helper function to update score for resulting array
+        Attributes:
+            frequency: db frequency result
+            weight: the weight for the current call
+            iwords: keywords in search engine
+            target_dict: dictionary to update
+        """
+        for word in words:
+            if word in frequency:
+                for key, value in frequency[word].items():
+                    score = target_dict.get(key, 0) + value * weight
+                    if score != 0:
+                        target_dict[key] = score
+
+    def _search_query(self, keywords, synonyms):
+        """
+        Counts the number of incidences between article words, keywords, and synonyms
+        implementing the different weights for the different type of weight values
+
+        Returns:
+            A map of the score for every article
+        """
+
+        score_per_article = {}
+
+        # keywords_json = {"keywords": keywords}
+        # synonyms_json = {"keywords": synonyms}
+        # article_keywords_frequency = requests.post(db_endpoint, json=keywords_json)
+        # article_synonyms_frequency = requests.post(db_endpoint, json=synonyms_json)
+
+        article_keywords_frequency = testing.test_constants.KEYWORDS_DB_MOCK_1
+        article_synonyms_frequency = testing.test_constants.SYNONYMS_DB_MOCK_1
+
+        self._calculate_score(article_keywords_frequency, self.keywords_weight,
+                              keywords, score_per_article)
+        self._calculate_score(article_synonyms_frequency, self.synonyms_weight,
+                              synonyms, score_per_article)
+
+        return score_per_article
