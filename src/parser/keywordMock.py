b6fc606dbd36a00c6f43fef937c39ae9c2e17ed2
Hisao <jaimehisao2011@gmail.com>
2020-08-13 02:34:34 +0000

    LintFix4

--- src/parser/keywordMock.py
+++ src/parser/keywordMock.py
@@ -4,5 +4,10 @@ import json
 
 def get_keywords(text):
     splited_text = text.split()
-    keywords = {"keywords": [splited_text[random.randint(0, len(splited_text)-1)], splited_text[random.randint(0, len(splited_text)-1)]]}
+    keywords = {
+        "keywords": [
+            splited_text[random.randint(0, len(splited_text) - 1)],
+            splited_text[random.randint(0, len(splited_text) - 1)],
+        ]
+    }
     return json.dumps(keywords)
