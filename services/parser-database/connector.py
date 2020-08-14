58d5eeed1cc1b284223713aac91d6338d17e4726
Hisao <jaimehisao2011@gmail.com>
2020-08-14 21:07:36 +0000

    Retriever file modification
    Modified dockerfile for default value
    
    CHANGED PRINTS TO LOGGING
    
    nit fix on parser
    
    Fixed output to match Javier's serivce
    
    Removed extra print statements
    
    Added connector.py to be in charge of all interactions w/databases and services
    
    Adapted files to interact w/connector.py
    
    Fixed UTF enconding error when returning JSON
    
    Modified Tests and added HTTP endpoint tests
    
    Removed extras stuff from parser
    
    Corrected API SPEC
    
    PYLINT
    
    rm files
    
    rm files
    
    rm files
    
    connected to service
    
    pylint
    
    rm extra file

---
+++ services/parser-database/connector.py
@@ -0,0 +1,70 @@
+"""conector.py - One stop connector for external
+services/databases"""
+import os
+import requests  # pylint: disable=import-error
+
+
+articles_in_memory = {}
+keywords_in_memory = {}
+
+
+def get_documents_to_parse():
+    # When database is integrated, this will go away
+    mty_document = {
+        "hash":
+        "afafbfbdce8c40924edae00f6ce54f0c639ce42a2" +
+        "c0fbbfa6ab82ea6925827c51",
+        "jurisdiction":
+        "Monterrey",
+        "url":
+        "http://www.guadalupe.gob.mx/wp-content/up" +
+        "loads/2019/09/Nuevo-Reglamento-Homologado-1.pdf",
+    }
+    document_list = []
+    document_list.append(mty_document)
+    return document_list
+
+
+def get_keywords(text):
+    """Get keywords that relate to this article (Javier's service)
+
+    Args:
+        text (sting): text to extract keywords from
+
+    Returns:
+        [list]: list of extracted keywords
+    """
+
+    extracted_keywords = []
+    request = {'text': text}
+    nlp_output = requests.post(os.getenv("KEYWORDS_SERVICE"), json=request)
+    # nlp_output = keywordmock.get_keywords(text)
+    json_output = nlp_output.json()
+    for keyword in json_output["tokens"]:
+        extracted_keywords.append(keyword["lemma"])
+    return extracted_keywords
+
+
+def get_article_by_number(art_num):
+    if art_num in articles_in_memory:
+        return articles_in_memory[art_num]
+    return None
+
+
+def get_articles_that_match_keywords(keywords_list):
+    """Returns articles that match the the given keyword(s)
+
+    Args:
+        keywords_list (list): Keyword(s) to look for
+
+    Returns:
+        list: articles that match such keyword(s)
+    """
+    matching_articles = {}
+    for keyword in keywords_list:
+        articles_that_match_keyword = {}
+        if keyword in keywords_in_memory:
+            for article in keywords_in_memory[keyword]:
+                articles_that_match_keyword[article["articleNumber"]] = article["frequency"]
+        matching_articles[keyword] = articles_that_match_keyword
+    return matching_articles
--- services/parser-database/retriever.py
+++
@@ -1,8 +0,0 @@
-import requests  # pylint: disable=import-error
-
-
-def get_document(url):
-    """Retrieves a PDF document from the specified URL
-    Saves it as regs.pdf"""
-    response = requests.get(url)
-    open('regs.pdf', 'wb').write(response.content)
