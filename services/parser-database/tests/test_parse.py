ca1b002457e60867148f8b5d396239a303afc337
Hisao <jaimehisao2011@gmail.com>
2020-08-14 02:19:06 +0000

    ADDED TESTS WUWUWUWU

---
+++ services/parser-database/tests/test_parse.py
@@ -0,0 +1,46 @@
+import pytest
+
+from parser import parse
+from parser import identify_articles
+from parser import get_article_by_number
+from parser import article_to_dictionary
+
+
+test_file = {
+    "hash":
+    "afafbfbdce8c40924edae00f6ce54f0c639ce42a2" +
+    "c0fbbfa6ab82ea6925827c51",
+    "jurisdiction": "tests/test_document",
+}
+
+'''
+def test_file_parsability():
+    parse(test_file)
+'''
+
+articles = (
+    "articulo 1.- los ciclistas se deben de orillar"
+    + " articulo 2- los ciclistas no se deben de orillar"
+    + " articulo 3. velocidad maxima de 10kmh"
+    + " artículo 4.- carros de mas de 3/4 toneladas requieren licencia de chofer"
+    + " artículo 5.- no te pueden infraccionar en navidad"
+    + " artículo 6.- marcar al seguro y al 911 al chocar"
+)
+
+
+def test_article_extraction_by_number_of_articles_extracted():
+    recieved_articles = identify_articles(articles.strip().split())
+    assert len(recieved_articles) == 6
+
+
+def test_article_extraction_by_article_content():
+    recieved_articles = identify_articles(articles.strip().split())
+    assert recieved_articles[1].text == " los ciclistas se deben de orillar"
+    assert recieved_articles[4].text == " carros de mas de 3/4 toneladas requieren licencia de chofer"
+
+
+def test_article_retrieval():
+    recieved_articles = identify_articles(articles.split())
+    for article in recieved_articles:
+        article_to_dictionary(article)
+    assert get_article_by_number(2) == "los ciclistas no se deben de orillar"
