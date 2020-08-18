f7ea3cb3a3ca2e8574afaa1bbb65f45a6f42b52d
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-18 17:11:26 -0500

    feature (error handling) Add error handling and logging on the frontend.
    + Added an `Alert` object to dynamically display errors in the frontend when necessary. This allows for better debugging and error tracking.
    
    + Added `@material-ui/lab` library to include `Alert` Material UI component.
    
    + Refactored articles/queries.js and its respective tests/queries.test.js files to support the error format normalization between services. As per agreement, bot `search` and `database` service now return errors in the following format:
    
    ```
    { error: { message: '', ... } }
    ```
    
    This allows a simpler error handling in the frontend.

--- server/tests/queries.test.js
+++ server/tests/queries.test.js
@@ -88,7 +88,7 @@ const tests = [
         `,
     variables: { search: '' },
     response: { data: { articles: ['20'] } },
-    expected: [new GraphQLError(JSON.stringify({ error: 'No article matches such ID', code: 404 }))]
+    expected: [new GraphQLError(JSON.stringify({ message: 'No article matches such ID', code: 404 }))]
   }
 ]
 
@@ -103,7 +103,7 @@ describe('Queries', () => {
       }
     }
 
-    return { error: 'No article matches such ID', code: 404 }
+    return { error: { message: 'No article matches such ID', code: 404 } }
   })
 
   tests.forEach(currentTest => {
