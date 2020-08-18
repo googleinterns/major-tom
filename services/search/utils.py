37656ce0236f90593fa99370a10e82ac81f33eec
Domene99 <jadomene99@gmail.com>
2020-08-18 13:48:26 -0500

    Wrapped requests futures functions

--- services/search/utils.py
+++ services/search/utils.py
@@ -2,6 +2,32 @@ from requests_futures.sessions import FuturesSession  # pylint: disable=import-e
 import constants
 
 
+class SynonymExtractor:
+    def __init__(self, word_arr, max_synonyms):
+        self.word_arr = word_arr
+        self.max_synonyms = max_synonyms
+        self.session = FuturesSession()
+        self.reqs = self.create_conc_reqs()
+
+    def create_conc_reqs(self):
+        """
+        create an asynchronous request list using requests-futures
+        Attributes:
+            word_arr: A list of words which each is a different request
+            session: A requests-futures session
+        Return:
+            A list of requests for every word
+        """
+        reqs = []
+        for word in self.word_arr:
+            reqs.append(self.session.get(constants.SPANISH_API_URL+word))  # pylint: disable=no-member
+
+        return reqs
+
+    def req_to_json(self, json):
+        return json.result().json()
+
+
 def create_synonym_list_esp(word_arr, max_synonyms=5):
     """
     Retrieves a synonym list given a word list
@@ -11,12 +37,14 @@ def create_synonym_list_esp(word_arr, max_synonyms=5):
             optional: Default value 5
     Returns: a list of synonyms
     """
-    session = FuturesSession()
+    #session = FuturesSession()
     synonyms = []
-    reqs = create_conc_reqs(word_arr, session)
+    #reqs = create_conc_reqs(word_arr, session)
 
-    for req in reqs:
-        resp = req.result().json()
+    synonym_extractor = SynonymExtractor(word_arr, max_synonyms)
+
+    for req in synonym_extractor.reqs:
+        resp = synonym_extractor.req_to_json(req)
 
         if 'sinonimos' not in resp:
             raise Exception("Unexpected synonyms API response format")
