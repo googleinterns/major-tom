47eb70df5a417b366ba15f774433d37e1966d849
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-25 17:39:26 -0500

    enhancement (welcome-message) Add welcome message and fix display styles.
    + Added a more descriptive placeholder for the search bar. [components/search-bar/index.js]
    + Modified CSS margin attributes for Search Bar to fit better the new results counter. [components/search-bar/styles.js]
    + Added a new Welcome message card that displays whenever there are no articles to display. The card introduces the user as to what the application does and an example to start using the service. [views/search-engine/index.js, views/search-engine/styles.js]

--- app/src/views/search-engine/index.js
+++ app/src/views/search-engine/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { useDebounce } from 'use-lodash-debounce'
-import { Container, Snackbar } from '@material-ui/core'
+import { Card, CardContent, Container, Snackbar, Chip, Typography } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 import SearchBar from './components/search-bar'
 import ArticlesList from './components/articles-list'
 import { GET_ARTICLES } from './graphql/queries'
+import styles from './styles'
 
 const SearchEngine = () => {
   const [loading, setLoading] = useState(false)
@@ -14,6 +15,8 @@ const SearchEngine = () => {
   const [search, setSearch] = useState('')
   const debouncedSearch = useDebounce(search, 1000)
   const getArticlesQuery = useQuery(GET_ARTICLES, { variables: { search: debouncedSearch } })
+  const classes = styles()
+  const DOC_URL = 'https://www.guadalupe.gob.mx/nuevo-reglamento-de-transito-homologado/'
 
   useEffect(() => {
     const { data, error } = getArticlesQuery
@@ -44,7 +47,48 @@ const SearchEngine = () => {
         setLoading={setLoading}
         loading={loading}
       />
+      <Chip
+        size='small'
+        label={`${articles.length} resultados`}
+        className={classes.resultsChip}
+      />
       <ArticlesList articles={articles} />
+      {articles.length === 0 &&
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+                Motor de búsqueda - Reglamento Vial de Monterrey
+            </Typography>
+            <Typography
+              variant='body1'
+              color='textSecondary'
+              component='p'
+              className={classes.cardContent}
+            >
+                Algoritmo para realizar búsquedas optimizadas sobre los <b>reglamentos viales oficiales</b> de tu comunidad.
+                El motor de búsqueda intentará encontrar los artículos <u>más relevantes para tu búsqueda</u>.
+            </Typography>
+            <Typography
+              variant='body1'
+              color='textSecondary'
+              component='p'
+              className={classes.cardContent}
+            >
+                Intenta buscar: <mark>Licencia de conducir</mark>
+            </Typography>
+            <Typography
+              variant='body2'
+              color='textSecondary'
+              component='p'
+              className={classes.cardContent}
+            >
+                Ahorita estás buscando en:
+            </Typography>
+            <a href={DOC_URL} target='_blank' rel='noopener noreferrer'>
+                REGLAMENTO DE TRÁNSITO Y VIALIDAD DEL MUNICIPIO DE GUADALUPE, NUEVO LEÓN
+            </a>
+          </CardContent>
+        </Card>}
       <Snackbar
         open={error !== undefined}
         autoHideDuration={2000}
