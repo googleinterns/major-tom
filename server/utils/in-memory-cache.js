9b66dfb762d5a88b565d1e57848405894049be7e
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-25 14:08:46 -0500

    bugfix (cache) Add expiration to cached articles to flush them periodically.
    + Added `.expire` to every article set to last at most a day on the cache before being flushed. [resolvers/article/index.js]
    
    + Had to switch Redis dependency from `async-redis` to `redis` since the first one hasn't been updated in a while and lacks expiration support. Plus `redis` library is the official one suggested on Redis documentation. [package.json, yarn.lock]
    
    + The `redis` dependency comes with the hassle of not supporting asynchronous calls out of the box, but, as per their documentation, in order to force it to be asynchronous, the built-in `util` library from Node.js allows us to wrap the Redis ops in asynchronous calls. [index.js]

--- server/utils/in-memory-cache.js
+++ server/utils/in-memory-cache.js
@@ -1,6 +1,7 @@
 const localCache = new Map()
 
 localCache.exists = key => localCache.has(key)
+localCache.expire = (key, ttl) => setTimeout(() => localCache.delete(key), ttl * 1000)
 localCache.on = (_, connection) => connection('🧶  No local Redis instance found... Defaulting to an in-memory cache storage.')
 
 export default localCache
